83c5de7c525abd84537c3348f0d6fee6e55f13e5
Florian Hofmann <fho@f12n.de>
2015-05-21 12:37:32 +0200

    inlined most functions in SimpleMat

--- src/Data/SimpleMat.hs
+++ src/Data/SimpleMat.hs
@@ -11,18 +11,19 @@ import           GHC.TypeLits
 import           GHC.Stack
 
 import qualified Data.Vector as V
+
 import           Data.Proxy
 import           Data.List
 import           Data.List.Split
 
-import           Debug.Trace
+{-import           Debug.Trace-}
 
 
 newtype Mat (w :: Nat) (h :: Nat) a = Mat { unMat :: V.Vector a } deriving (Show,Read)
 
 instance Functor (Mat w h) where
     fmap f (Mat v) = Mat $ V.map f v
-
+    {-# INLINABLE fmap #-}
 
 instance Num a => Num (Mat (w :: Nat) (h :: Nat) a) where
     (+) (Mat a) (Mat b) = Mat $ V.zipWith (+) a b
@@ -32,6 +33,13 @@ instance Num a => Num (Mat (w :: Nat) (h :: Nat) a) where
     abs    (Mat a)      = Mat $ V.map abs a
     signum (Mat a)      = Mat $ V.map signum a
     fromInteger a       = Mat $ V.replicate 64 (fromInteger a)
+    {-# INLINABLE (+) #-}
+    {-# INLINABLE (-) #-}
+    {-# INLINABLE (*) #-}
+    {-# INLINABLE negate #-}
+    {-# INLINABLE abs #-}
+    {-# INLINABLE signum #-}
+    {-# INLINABLE fromInteger #-}
 
 scale :: Num a => a -> Mat w h a -> Mat w h a 
 scale f = Mat . V.map (*f) . unMat
@@ -39,6 +47,7 @@ scale f = Mat . V.map (*f) . unMat
 
 norm :: Floating c => Mat t t1 c -> c
 norm (Mat a) = sqrt . V.sum $ V.zipWith (*) a a
+{-# INLINABLE norm #-}
 
 variance :: Fractional a => Mat w h a -> a
 variance (Mat a) = vecVariance a
@@ -54,9 +63,11 @@ vecMean a = V.sum a / fromIntegral (V.length a)
 
 sumElems' :: Num a => Mat w h a -> a
 sumElems' = V.sum . unMat
+{-# INLINABLE sumElems' #-}
 
 zipWith' :: (a -> b -> c) -> Mat w h a -> Mat w h b -> Mat w h c
 zipWith' f (Mat a) (Mat b) = Mat $ V.zipWith f a b
+{-# INLINABLE zipWith' #-}
 
 fromList' :: [a] -> Mat w h a
 fromList' l = Mat $ V.fromList l
@@ -66,26 +77,31 @@ mkMat as | s == length as = m
          | otherwise      = error "invalid matrix size"
   where m = Mat $ V.fromListN s as
         s = width m * height m
-
+{-# INLINABLE mkMat #-}
 
 width :: forall w h a. KnownNat w => Mat w h a -> Int
 width _ = fromInteger $ natVal (Proxy :: Proxy w)
+{-# INLINABLE width #-}
 
 height :: forall w h a. KnownNat h => Mat w h a -> Int
 height _ = fromInteger $ natVal (Proxy :: Proxy h)
+{-# INLINABLE height #-}
 
 index :: forall w h a. KnownNat w => Mat w h a -> Int -> Int -> a
 index m@(Mat v) x y = v V.! ( x + y * width m)
+{-# INLINABLE index #-}
 
 
 
 row :: KnownNat w => Int -> Mat w h a -> V.Vector a 
 row y m@(Mat v) = V.slice (y*w) w v
   where w = width m
+{-# INLINABLE row #-}
 
 rows :: (KnownNat w, KnownNat h) => Mat w h a -> [V.Vector a]
-rows m@(Mat v) = [ row y m | y <- [0..h-1]]
+rows m@(Mat _) = [ row y m | y <- [0..h-1]]
   where h = height m
+{-# INLINABLE rows #-}
 
 subMat :: (KnownNat w, KnownNat h, KnownNat w', KnownNat h') 
        => Int -> Int -> Mat w h a -> Mat w' h' a
@@ -97,6 +113,7 @@ subMat ox oy m@(Mat v) | valid     = newM
           newH = height newM
           oldW = width m
           oldH = height m
+{-# INLINABLE subMat #-}
 
 --------------------------------------------------
 
